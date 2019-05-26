3fbd8cd4ad3f03980e54bee5cc8c78d7c303ddf9
whisper540 <whisper_540@yahoo.com>
2019-05-26 09:07:56 +0800

    redefine

--- src/store/constants.js
+++
@@ -1,8 +0,0 @@
-const constants = {
-    SORT_COLORS: "SORT_COLOR",
-    ADD_COLOR: "ADD_COLOR",
-    RATE_COLOR: "RATE_COLOR",
-    REMOVE_COLOR: "REMOVE_COLOR"
-};
-
-export default constants;
--- src/store/store.js
+++
@@ -1,7 +0,0 @@
-import { createStore, combineReducers } from 'redux';
-import { colors, sort } from './reducers.js';
-
-const store = createStore(
-    combineReducers({ colors, sort })
-);
-
